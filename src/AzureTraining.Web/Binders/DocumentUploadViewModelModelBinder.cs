15a620629a3793dfb4b412be39624f1974d02d44
Bogdan Dudnik <Bogdan.Dudnik@dataart.com>
2013-04-17 10:40:52 +0300

    cleanup for file upload page

---
+++ src/AzureTraining.Web/Binders/DocumentUploadViewModelModelBinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureTraining.Web.Binders
+{
+    class DocumentUploadViewModelModelBinder : DefaultModelBinder
+    {
+        public static byte[] GetBytes(Stream input)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                input.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+        
+        protected override void BindProperty(ControllerContext controllerContext, ModelBindingContext bindingContext, System.ComponentModel.PropertyDescriptor propertyDescriptor)
+        {
+            if (propertyDescriptor.Name == "Content")
+            {
+                try 
+                {
+                    base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
+                }
+                catch (HttpRequestValidationException)
+                {
+                    bindingContext.ModelState.AddModelError("Content", "File content doesn't looks like text file");
+                }
+                return;
+            }
+            base.BindProperty(controllerContext, bindingContext, propertyDescriptor);
+        }
+    }
+}
---
+++ src/AzureTraining.Web/Controllers/DocumentUploadController.cs
@@ -0,0 +1,52 @@
+﻿using AzureTraining.Core;
+using AzureTraining.Core.WindowsAzure;
+using AzureTraining.Web.Models;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace AzureTraining.Web.Controllers
+{
+    public partial class DocumentUploadController : Controller
+    {
+        private readonly IDocumentRepository _repository;
+
+        public DocumentUploadController(IDocumentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public DocumentUploadController()
+            : this(new DocumentRepository())
+        {
+            
+        }
+        public virtual ActionResult Upload()
+        {
+            DocumentUploadViewModel documentViewModel = new DocumentUploadViewModel { };
+            return View(documentViewModel);
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        [Authorize]
+        public virtual ActionResult Upload(DocumentUploadViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            string owner = User.Identity.Name.ToLowerInvariant();
+
+            _repository.Add(new Document
+            {
+                Name = model.Name,
+                Owner = owner,
+                IsShared = model.IsShared
+            }, model.Content);
+
+            return RedirectToAction("Upload", "Home");
+        }
+
+    }
+}
